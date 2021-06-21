997e42ac595c685de55f3d5491e8921e05da2e59
Francesco Pupillo <pupillo@psych.uni-frankfurt.de>
2021-06-21 10:43:19 +0200

    changed scripts to include irr for different raters, and between one rater and correct slices; modified the readme.md file

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # hippocampal_ranging
 
-Calculate the interrater reliability for the hippocampal ranging process
+Calculate the interrater reliability for the hippocampal ranging process, using
+test sets and correct slices from the (HPC group)[https://gitlab.com/viraganna.varga/hippocampal-subfield-ranging/-/wikis/home/The-Hippocampal-Ranging-Process].
+
+The script irr.R calculates the inter-rater reliability of the hippocampal segmentations, comparing 
+ratings of starting and ending slices (right and left) with either correct slices (from the HPC group) or two independent raters. 
+
+It requires to add the names of the file in the rater1 and rater2 folders in the following format:
+`rater<1 or 2>.<test set>.<round>.`
+For example, the the first round of ratings from the first set of images should be added in the following format:
+`rater1.1.1` for rater 1; `rater2.1.1` for rater 2. 
+
+The correct slices are already in the `correct.slices` folder. 
 
 Description of the files:
+### correct.slices
+.csv files that are indicated as correct from the (Hippocampal Ranging Group)[https://gitlab.com/viraganna.varga/hippocampal-subfield-ranging/-/wikis/home/The-Hippocampal-Ranging-Process]
 ### rater1
 .csv files containing information about the ranging for rater1 testsets
 ### test_sets
--- irr.R
+++ irr.R
@@ -9,19 +9,20 @@
 
 rm(list = ls())
 
+
 # getting the neccessary libs
 library(irr)
 library(reshape)
 library(ggplot2)
 
 # function to calculate the kappa
-KappaCalc<-function(test.set, correct){
+KappaCalc<-function(rater1, rater2){
   
-  # calculating kappas of interest
-  StartLeftKappa <- kappa2(cbind(test.set$Starting.slice.left,correct$Starting.slice.left))
-  StartRightKappa <-kappa2(cbind(test.set$Starting.slice.right,correct$Starting.slice.right))
-  EndLeftKappa <-kappa2(cbind(test.set$end.slice.left,correct$end.slice.left))
-  EndRightKappa <-kappa2(cbind(test.set$end.slice.right,correct$end.slice.right))
+  # calculating kappas of interest 
+  StartLeftKappa <- kappa2(cbind(rater1$Starting.slice.left,rater2$Starting.slice.left))
+  StartRightKappa <-kappa2(cbind(rater1$Starting.slice.right,rater2$Starting.slice.right))
+  EndLeftKappa <-kappa2(cbind(rater1$end.slice.left,rater2$end.slice.left))
+  EndRightKappa <-kappa2(cbind(rater1$end.slice.right,rater2$end.slice.right))
   
   # writing these in a vector
   AllKappas <- cbind(StartLeftKappa$value,StartRightKappa$value,EndLeftKappa$value,EndRightKappa$value)
@@ -29,49 +30,89 @@ KappaCalc<-function(test.set, correct){
   return(AllKappas)
 }
 
+# compare against correct slices or with another rater?
+# 1: correct slices 
+# 2: another rater
+whichComp<-2
+
 # list all the test sets
-testsets<-list.files("test_sets")
+rater1<-list.files("rater1")
 
 # load data
 cd<-getwd() # store the current path in a variable
-setwd("test_sets") # navigate to the folder where the test sets are
-for (n in 1:length(testsets)){
-  assign(paste("test.set", n, sep=""), read.csv(testsets[n])) # load each test set and store in a variable
+setwd("rater1") # navigate to the folder where the test sets are
+for (n in 1:length(rater1)){
+  # extract the test set from name of the file
+  testset<- as.numeric(substr(rater1[n], 8,8))
+  # extract the round
+  round<-as.numeric(substr(rater1[n], 10,10))
+  # create a dataset
+  assign(paste("rater1.", testset,".",round, sep=""), read.csv(rater1[n])) # load each test set and store in a variable
 }
 setwd(cd) # return to the previous folder
 
-# load correct slices
-# load data
+# load correct slices or rater two slices
 
 # list all the correct slices sets
-correctslices<-list.files("correct_slices")
+if (whichComp==1){
+  
+rater2<-list.files("correct.slices")
 
-setwd("correct_slices")
-for (n in 1:length(correctslices)){
-  assign(paste("Correct-slices", n, sep=""), read.csv(correctslices[n]))
+setwd("correct.slices")
+
+for (n in 1:length(rater2)){
+
+  assign(paste("rater2.", n, sep=""), read.csv(rater2[n]))
 }
 setwd(cd)
 
+} else if (whichComp==2){
+  
+  rater2<-list.files("rater2")
+  
+  setwd("rater2")
+  
+  for (n in 1:length(rater2)){
+    
+    # extract test set
+    testset<-as.numeric(substr(rater2[n], 8,8))
+    
+    # extract the round
+      round<-as.numeric(substr(rater2[n], 10,10))
+    
+    assign(paste("rater2.", testset, ".", round, sep=""), read.csv(rater2[n]))
+  }
+  setwd(cd)
+  
+}
+
 # calculate the kappa
-files<-ls(pattern ="^test.set")
+files<-ls(pattern ="^rater1.")
 
 for (n in files){
+  
   # get the set number
-  setN<-substr(n, 9, 10)
+  setN<-substr(n, 8, 8)
+  # extract the round
+
+  round<-as.numeric(substr(n, 10,10))
   
-  curr_test_set<-get(n) # the test set
+  curr_rater1<-get(n) 
   
-  curr_corr_slices<-get(paste("Correct-slices", setN, sep="")) # the corresponsed correct slice
-  
+  if (whichComp==1){ # it we are comparing against correct slices
+  curr_rater2<-get(paste("rater2.", setN, sep="")) # the corresponsed correct slice
+  } else { # if we are comparing against other rather
+    curr_rater2<-get(paste("rater2.", setN, ".", round, sep="")) # the corresponsed correct slice
+  }
 assign(paste("kappa_",n, sep=""), 
-       KappaCalc(curr_test_set, curr_corr_slices))
+       KappaCalc(curr_rater1, curr_rater2))
 }
 
 
 # plot 
 # bind the kappa
 kappaAll<-vector()
-for (n in ls(pattern="^kappa_test")){
+for (n in ls(pattern="^kappa_rater")){
   
 curr_kappa<-data.frame(get(n))
 
@@ -81,19 +122,40 @@ kappaAll<-data.frame(rbind(kappaAll, curr_kappa))
 
 }
 
+# get the round 
+# and the testset
+round<-vector()
+testset<-vector()
+kappas<-ls(pattern="^kappa_rater")
+
+for(n in 1: length(kappas)){
+  round[n]<-as.numeric(substr(kappas[n], nchar(kappas[n]), nchar(kappas[n])))
+  testset[n]<-as.numeric(substr(kappas[n], 14,14))
+  
+}
+
 # create test set variable
-kappaAll$testset<-as.factor(1:nrow(kappaAll))
+kappaAll$round<-round
+kappaAll$testset<-testset
+
+# sort
+kappaAll<-kappaAll[order(kappaAll$round, kappaAll$testset),]
+
+# create progressive variable
+kappaAll$testN<-1:nrow(kappaAll)
 
 # merge
-kappamer<-melt(kappaAll, id.vars="testset")
+kappamer<-melt(kappaAll[,c(1:4,7)], id.vars="testN")
+names(kappamer)[2:3]<-c("Slice", "IRR")
 
 # plot
-ggplot(kappamer, aes(x=testset, y=value, group = variable, colour=variable))+
+print(
+ggplot(kappamer, aes(x=testN, y=IRR, group = Slice, colour=Slice))+
   geom_line()+
   geom_point()+
-  geom_text(aes(label = round(value, 2)),
+  geom_text(aes(label = round(IRR, 2)),
             vjust = "outward", hjust = "inward",
             show.legend = FALSE, colour = "black") 
-
+)
 # save the plot
 ggsave( "irr.jpg", plot = last_plot())
